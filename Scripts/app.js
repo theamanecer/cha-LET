5f6564fdd521b2b52e6fecd146cd3ceaeb1c5954
pramod <pramod.madapati@outlook.com>
2019-03-09 00:20:13 -0600

    my first commit

---
+++ Scripts/app.js
@@ -0,0 +1,14 @@
+﻿var app = angular.module('myapp', []);
+app.controller('empcont', function ($scope) {
+    $scope.myvar = 'Hello';
+
+    $scope.myfunc = function () {
+                    alert('method call');
+    };
+    $scope.myfunc2 = function (val, val2, val3) {
+        alert('welcome' + val + val2 + val3);
+        $scope.myval = val;
+        $scope.myval2 = val2;
+        $scope.myval3 = val3;
+    };
+    });
